4c4e0036631f43d15232485d79124fbde79cc2bb
Danil <121952873+bababanana1995@users.noreply.github.com>
2023-04-09 12:22:50 +0700

    1

--- components/header.js
+++ components/header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const Header = ()=> {
+const Header = () => {
     return (
-        
+        <header className="header">
+            <div className="Logo"><img src="https://i.pinimg.com/originals/e8/7a/fb/e87afb485ae35f89aceef01bc65ff1c0.png"></img></div>
+            <div className='footer-text'><p className='pText'>сразу после картинки очень длинный текст в целом я его могу писать прямо до
+                конца строки даже не думал что это может занять столько символов е всё равно мало нужно ещё немножко</p>
+            </div>
+        </header>
     )
 }
---
+++ components/profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className="main-content">
+            <div>
+                <img src=""></img>
+            </div>
+            <div>ava + description</div>
+            <div>my post</div>
+            <div>
+                <div>Post1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+}
+export default Profile
