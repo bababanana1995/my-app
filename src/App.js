0ae2fd25209b3d679cd4463336cf281480f92a6f
Danil <121952873+bababanana1995@users.noreply.github.com>
2023-04-10 22:19:08 +0700

    d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
-import Navigation from './components/navigation';
-import Profile from './components/profile';
+import Header from './components/Header/header';
+import Navigation from './components/Nav/navigation';
+import Profile from './components/Profile/profile';
 
 function App() {
   return (
     <div className="app_wrapper">
       <Header />
+      <div className="fl">
       <Navigation />
       <Profile />
+      </div>
     </div>
   );
 }
---
+++ src/components/Profile/profile.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import s from './profile.module.css'
+import MyPost from './myPost/Post/MyPost'
+
+const Profile = () => {
+    return (
+
+        <div className={s.main_content}>
+            {/* <div className={s.position_i}>
+                <img className={s.big_i} src="https://otrisovki.ru/wp-content/uploads/2021/04/9907.jpg"></img>
+            </div> */}
+
+            <div className={s.ava_text}>
+                <div className={s.position_ava}>
+                    <img className={s.ava} src='https://amiel.club/uploads/posts/2022-03/1647748258_34-amiel-club-p-multyashnie-kartinki-na-avatarku-35.jpg'></img>
+                </div>
+                <div className={s.position_text}>
+                    <p className={s.text}>Name: Алиса Донаван</p>
+                    <p className={s.text}>Age: 25 лет</p>
+                    <p className={s.text}>City: Санкт-Петербург</p>
+                    <p className={s.text}>About: Люблю вышивать крестиком и пить очень вкусное вино</p>
+                </div>
+            </div>
+            <div className={s.position_posts}>
+                <div className={s.post_h}><h1>My post</h1></div>
+                <div><input className={s.typ} type="text" ></input></div>
+                <button className={s.knopka}>Жмяк и всё</button>
+                <MyPost />
+
+            </div>
+        </div>
+
+    )
+}
+export default Profile
